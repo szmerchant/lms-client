6931abee7f8211108f516061263413905eb75cef
Saman Merchant <samanzmerchant@gmail.com>
2024-11-26 14:09:31 -0600

    custom next dev proxy server

--- pages/register.js
+++ pages/register.js
@@ -13,7 +13,7 @@ const Register = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API}/register`, {
+            const { data } = await axios.post("/api/register", {
                 name,
                 email,
                 password
