221aed98ce5913eb98816df9d21de1b55fab2f41
Saman Merchant <samanzmerchant@gmail.com>
2024-11-26 14:33:03 -0600

    active nav links + link redirect to login from register

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,29 +1,40 @@
+import { useState, useEffect } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
 import { AppstoreOutlined, LoginOutlined, UserAddOutlined } from '@ant-design/icons';
-
-const items = [
-    {
-      key: "app",
-      icon: <AppstoreOutlined />,
-      label: <Link href="/">App</Link>,
-    },
-    {
-      key: "login",
-      icon: <LoginOutlined />,
-      label: <Link href="/login">Login</Link>,
-    },
-    {
-      key: "register",
-      icon: <UserAddOutlined />,
-      label: <Link href="/register">Register</Link>,
-    },
-];
   
 const TopNav = () => {
+    const [current, setCurrent] = useState("");
+
+    useEffect(() => {
+        typeof window !== "undefined" && setCurrent(window.location.pathname)
+        console.log(window.location.pathname);
+    }, [typeof window !== "undefined" && window.location.pathname]);
+
+    const items = [
+        {
+          key: "/",
+          onClick: (e) => setCurrent(e.key),
+          icon: <AppstoreOutlined />,
+          label: <Link href="/">App</Link>,
+        },
+        {
+          key: "/login",
+          onClick: (e) => setCurrent(e.key),
+          icon: <LoginOutlined />,
+          label: <Link href="/login">Login</Link>,
+        },
+        {
+          key: "/register",
+          onClick: (e) => setCurrent(e.key),
+          icon: <UserAddOutlined />,
+          label: <Link href="/register">Register</Link>,
+        },
+    ];
+
     return (
         <>
-            <Menu mode="horizontal" items={items} />
+            <Menu mode="horizontal" items={items} selectedKeys={[current]} />
         </>
     );
 };
--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const Register = () => {
     const [name, setName] = useState("");
@@ -63,6 +64,13 @@ const Register = () => {
                         {loading ? <SyncOutlined spin /> : "Submit"}
                     </button>
                 </form>
+
+                <p className="text-center p-3">
+                    Already registered?{" "}
+                    <Link href="/login">
+                        Login
+                    </Link>
+                </p>
             </div>
         </>
     );
