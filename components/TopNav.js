22ac8716985d7b5567cd11dd40347ee28ae37592
Saman Merchant <samanzmerchant@gmail.com>
2024-11-27 16:16:28 -0600

    global state, login/logout

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,34 +1,70 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
-import { AppstoreOutlined, LoginOutlined, UserAddOutlined } from '@ant-design/icons';
+import {
+  AppstoreOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  UserAddOutlined
+} from '@ant-design/icons';
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
   
 const TopNav = () => {
     const [current, setCurrent] = useState("");
+
+    const { state, dispatch } = useContext(Context);
+
+    const { user } = state;
+
+    const router = useRouter();
 
     useEffect(() => {
         typeof window !== "undefined" && setCurrent(window.location.pathname)
     }, [typeof window !== "undefined" && window.location.pathname]);
 
+    const logout = async () => {
+        dispatch({ type: "LOGOUT" });
+        window.localStorage.removeItem("user");
+        const { data } = await axios.get("/api/logout");
+        toast(data.message);
+        router.push("/login");
+    };
+
+    // Dynamically filter menu items based on user state
     const items = [
-        {
-          key: "/",
-          onClick: (e) => setCurrent(e.key),
-          icon: <AppstoreOutlined />,
-          label: <Link href="/">App</Link>,
-        },
-        {
-          key: "/login",
-          onClick: (e) => setCurrent(e.key),
-          icon: <LoginOutlined />,
-          label: <Link href="/login">Login</Link>,
-        },
-        {
-          key: "/register",
-          onClick: (e) => setCurrent(e.key),
-          icon: <UserAddOutlined />,
-          label: <Link href="/register">Register</Link>,
-        },
+      {
+        key: "/",
+        onClick: (e) => setCurrent(e.key),
+        icon: <AppstoreOutlined />,
+        label: <Link href="/">App</Link>,
+      },
+      ...(!user
+        ? [
+            {
+              key: "/login",
+              onClick: (e) => setCurrent(e.key),
+              icon: <LoginOutlined />,
+              label: <Link href="/login">Login</Link>,
+            },
+            {
+              key: "/register",
+              onClick: (e) => setCurrent(e.key),
+              icon: <UserAddOutlined />,
+              label: <Link href="/register">Register</Link>,
+            },
+          ]
+        : [
+            {
+              key: "/logout",
+              onClick: logout,
+              icon: <LogoutOutlined />,
+              style: { marginLeft: "auto" }, // Push to the right
+              label: <>Logout</>,
+            },
+          ]),
     ];
 
     return (
--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
 import Link from "next/link";
+import { Context } from "../context";
+import { useRouter } from "next/router";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+
+    // state
+    const {state, dispatch} = useContext(Context);
+    // console.log("STATE", state);
+
+    // router
+    const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,9 +26,16 @@ const Login = () => {
                 email,
                 password
             });
-            // toast("Registration successful. Please login.");
-            // setLoading(false);
-            console.log("LOGIN RESPONSE", data);
+            // console.log("LOGIN RESPONSE", data);
+            dispatch({
+                type: "LOGIN",
+                payload: data,
+            });
+            // save in local storage
+            window.localStorage.setItem("user", JSON.stringify(data));
+            // redirect
+            router.push("/");
+            // setLoading(false)
         } catch (err) {
             toast(err.response.data);
             setLoading(false);
