0b5e40b5201e7b3f3840b796bfd569e0ab5fa305
Saman Merchant <samanzmerchant@gmail.com>
2024-12-06 19:14:05 -0600

    instructor courses in dashboard - json

--- pages/instructor/index.js
+++ pages/instructor/index.js
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 import InstructorRoute from "../../components/routes/InstructorRoute";
 
 const InstructorIndex = () => {
+    const [ courses, setCourses ] = useState([]);
+
+    useEffect(() => {
+        loadCourses();
+    }, []);
+
+    const loadCourses = async () => {
+        const { data } = await axios.get("/api/instructor-courses");
+        setCourses(data);
+    };
+
     return (
         <InstructorRoute>
             <h1 className="jumbotron text-center square py-5">Instructor Dashboard</h1>
+            <pre>{JSON.stringify(courses, null, 4)}</pre>
         </InstructorRoute>
     );
 };
