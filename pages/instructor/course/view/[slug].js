110435a84ec3dec6af43b3fd97155755c925eb96
Saman Merchant <samanzmerchant@gmail.com>
2024-12-06 21:07:24 -0600

    single course view: added description with markdown context display

--- pages/instructor/course/view/[slug].js
+++ pages/instructor/course/view/[slug].js
@@ -4,6 +4,7 @@ import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from "axios";
 import { Avatar, Tooltip } from "antd";
 import { EditOutlined, CheckOutlined } from "@ant-design/icons";
+import ReactMarkdown from "react-markdown";
 
 const CourseView = () => {
     const [ course, setCourse ] = useState({});
@@ -25,7 +26,7 @@ const CourseView = () => {
             <div className="container-fluid pt-3">
                 {course && (
                     <div className="container-fluid pt-1">
-                        <div className="d-flex align-items-center justify-content-between border-bottom pb-3">
+                        <div className="d-flex align-items-center justify-content-between pb-3">
                             {/* Avatar */}
                             <Avatar
                                 size={80}
@@ -51,6 +52,27 @@ const CourseView = () => {
                                 <Tooltip title="Publish">
                                     <CheckOutlined className="h5 pointer text-danger" />
                                 </Tooltip>
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        {/* Course Description */}
+                        <div className="row">
+                            <div className="col">
+                                <h6 className="mb-3 text-secondary">Course Description</h6>
+                                <div
+                                    style={{
+                                        padding: "10px",
+                                        border: "1px solid #ddd",
+                                        borderRadius: "5px",
+                                        backgroundColor: "#f9f9f9",
+                                        fontSize: "14px",
+                                        lineHeight: "1.6",
+                                    }}
+                                >
+                                    <ReactMarkdown>{course.description}</ReactMarkdown>
+                                </div>
                             </div>
                         </div>
                     </div>
