32ae713d93c761866f6827e4360eef195b9452cc
Saman Merchant <samanzmerchant@gmail.com>
2024-12-06 20:57:46 -0600

    single course view styling

--- pages/instructor/course/view/[slug].js
+++ pages/instructor/course/view/[slug].js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from "axios";
+import { Avatar, Tooltip } from "antd";
+import { EditOutlined, CheckOutlined } from "@ant-design/icons";
 
 const CourseView = () => {
     const [ course, setCourse ] = useState({});
@@ -21,7 +23,38 @@ const CourseView = () => {
     return (
         <InstructorRoute>
             <div className="container-fluid pt-3">
-                <pre>{JSON.stringify(course, null, 4)}</pre>
+                {course && (
+                    <div className="container-fluid pt-1">
+                        <div className="d-flex align-items-center justify-content-between border-bottom pb-3">
+                            {/* Avatar */}
+                            <Avatar
+                                size={80}
+                                src={course.image ? course.image.Location : "/course.png"}
+                            />
+
+                            {/* Course Details */}
+                            <div className="flex-grow-1 px-3" style={{ lineHeight: "1.4" }}>
+                                <h5 className="text-primary mb-2">{course.name}</h5>
+                                <p classsName="mb-1">
+                                    {course.lessons && course.lessons.length} Lessons
+                                </p>
+                                <p className="text-muted mb-0" style={{ fontSize: "12px", marginTop: "-5px" }}>
+                                    {course.category}
+                                </p>
+                            </div>
+
+                            {/* Action Icons */}
+                            <div className="d-flex align-items-center" style={{ gap: "20px" }}>
+                                <Tooltip title="Edit">
+                                    <EditOutlined className="h5 pointer text-warning" />
+                                </Tooltip>
+                                <Tooltip title="Publish">
+                                    <CheckOutlined className="h5 pointer text-danger" />
+                                </Tooltip>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </InstructorRoute>
     );
