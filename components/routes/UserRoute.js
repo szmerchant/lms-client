e7635b164297c805765f9c7061d17018b562af62
Saman Merchant <samanzmerchant@gmail.com>
2024-12-09 06:50:25 -0600

    stripe cancel page

--- components/routes/UserRoute.js
+++ components/routes/UserRoute.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { SyncOutlined } from "@ant-design/icons";
 import UserNav from "../nav/UserNav";
 
-const UserRoute = ({ children }) => {
+const UserRoute = ({ children, showNav = true }) => {
     // state
     const [ ok, setOk ] = useState(false);
     // router
@@ -37,7 +37,7 @@ const UserRoute = ({ children }) => {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-2">
-                            <UserNav />
+                            {showNav && <UserNav />}
                         </div>
                         <div className="col-md-10">{children}</div>
                     </div>
